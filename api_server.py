d712b0b302d9755a8b7d276b8d1d267e5278d765
Yunkyeong Jang <yunkyeong@ccmedia.co.kr>
2025-12-18 22:29:12 +0900

    chore: 깃헙 액션 설정 파일 추가

--- api_server.py
+++ api_server.py
@@ -43,6 +43,7 @@ app.add_middleware(
         "http://localhost:3001",
         "http://127.0.0.1:3000",
         "http://127.0.0.1:3001",
+        "http://codeninjax.gonetis.com:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
